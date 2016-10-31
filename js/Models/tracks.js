92cf36a9ed25ad235254c6c1cf532d8bf18f8e53
srgg6701 <srgg67@gmail.com>
2016-10-31 06:55:20 +0200

    Correcting tracks path.

--- js/Models/tracks.js
+++ js/Models/tracks.js
@@ -2,7 +2,7 @@ var tracksModel = Backbone.Model.extend({
     defaults: {
         tracks: function(){
             var deferred = new $.Deferred();
-            $.get('/contents/tracks/tracks.json', function(tracks){
+            $.get('contents/tracks/tracks.json', function(tracks){
                 //console.log({ this: this, tracks: tracks });
                 deferred.resolve(tracks);
             });
@@ -10,7 +10,7 @@ var tracksModel = Backbone.Model.extend({
         },
         loadTrack: function(track){
             var deferred = new $.Deferred();
-            $.get('/contents/tracks/'+track+'.mp3', function(track){
+            $.get('contents/tracks/'+track+'.mp3', function(track){
                 deferred.resolve(track);
             });
             return deferred.promise();
